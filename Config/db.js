616e5029c5d6a3135bac2e514d49e85ff7114859
Suraj-Dinesh-Gawade <gawadesurajdinesh@gmail.com>
2026-08-04 21:12:30 +0530

    Added database data from cloud

--- Config/db.js
+++ Config/db.js
@@ -7,7 +7,11 @@ const db = mysql.createConnection({
     host: process.env.DB_host,
     user: process.env.DB_user,
     password: process.env.DB_password,
-    database: process.env.DB_name
+    database: process.env.DB_name,
+     ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 });
 
 export default db;
